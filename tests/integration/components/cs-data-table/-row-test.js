e7d95a5c78d69df168c87cd025ecef98025688be
Nathanael Dekeister <nathanael_dekeister@ultimatesoftware.com>
2022-06-05 18:55:52 +0200

    test: -row component

--- tests/integration/components/cs-data-table/-row-test.js
+++ tests/integration/components/cs-data-table/-row-test.js
@@ -7,8 +7,12 @@ module('Integration | Component | cs-data-table/-row', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    await render(hbs`<CsDataTable::-Row />`);
+    await render(
+      hbs`<CsDataTable::-Row class="test" @index="1" as |row|></CsDataTable::-Row>`
+    );
 
-    assert.dom(this.element).hasText('');
+    assert
+      .dom('tr[data-test-row-index="1"]')
+      .hasClass('test', 'It pass attributes to tr');
   });
 });
